55d31fbdaf6d35e3971f21be28b99aee9452bac5
parkgang <ruddms936@naver.com>
2023-11-21 23:07:12 +0900

    feat: 10951

---
+++ src/baekjoon/bronze/10951-A+B - 4/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    while (scanf("%d %d", &a, &b) != -1)
+    {
+        printf("%d\n", a + b);
+    }
+
+    return 0;
+}
