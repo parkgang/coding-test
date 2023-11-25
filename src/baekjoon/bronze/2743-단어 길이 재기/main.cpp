3bf23900c5e6e398625be3eb3a98f075da4f034c
parkgang <ruddms936@naver.com>
2023-11-25 14:58:29 +0900

    feat: 2743

---
+++ src/baekjoon/bronze/2743-단어 길이 재기/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+
+    cin >> s;
+    cout << s.length();
+
+    return 0;
+}
