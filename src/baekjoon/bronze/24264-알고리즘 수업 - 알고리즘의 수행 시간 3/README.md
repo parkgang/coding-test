ce79c0ba924426b914700027255eedd9b4a27f2f
parkgang <ruddms936@naver.com>
2023-12-20 00:00:03 +0900

    feat(baekjoon): 24264

---
+++ src/baekjoon/bronze/24264-알고리즘 수업 - 알고리즘의 수행 시간 3/README.md
@@ -0,0 +1,11 @@
+## 문제
+
+https://www.acmicpc.net/problem/24264
+
+약 `10분` 소요
+
+## 실수
+
+`시간 복잡도` 는 구했는데 `input` 이 `int` 형으로 해도 문제가 없을 거 같아서 `int` 으로 했는데 `제곱` 을 구해야 해서 `long long` 인 것을 몰라서 틀렸었다.
+
+참고: https://carrot-farmer.tistory.com/54
---
+++ src/baekjoon/bronze/24264-알고리즘 수업 - 알고리즘의 수행 시간 3/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long n;
+
+    cin >> n;
+    cout << n * n << "\n"
+         << 2 << "\n";
+
+    return 0;
+}
