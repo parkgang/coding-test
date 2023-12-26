466d1e59f8f9b8ce357705175ef617ce58d3ff2b
parkgang <ruddms936@naver.com>
2023-12-26 12:09:33 +0900

    feat(baekjoon): 19532

---
+++ src/baekjoon/bronze/19532-수학은 비대면강의입니다/README.md
@@ -0,0 +1,5 @@
+## 문제
+
+https://www.acmicpc.net/problem/19532
+
+`11분 30초` 소요
---
+++ src/baekjoon/bronze/19532-수학은 비대면강의입니다/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, b, c, d, e, f;
+
+    cin >> a >> b >> c >> d >> e >> f;
+
+    for (int x = -999; x <= 999; x++)
+        for (int y = -999; y <= 999; y++)
+            if (
+                a * x + b * y == c &&
+                d * x + e * y == f)
+            {
+                cout << x << " " << y;
+                return 0;
+            }
+
+    return 0;
+}
