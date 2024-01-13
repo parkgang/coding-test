8141fdf353b5643dc07d20c8741a1cfccf6647a2
parkgang <ruddms936@naver.com>
2024-01-13 14:43:57 +0900

    feat: 1620

---
+++ src/baekjoon/silver/1620-나는야 포켓몬 마스터 이다솜/README.md
@@ -0,0 +1,7 @@
+## 문제
+
+https://www.acmicpc.net/problem/1620
+
+## 풀이시간
+
+16분
---
+++ src/baekjoon/silver/1620-나는야 포켓몬 마스터 이다솜/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int N, M;
+  cin >> N >> M;
+
+  vector<string> vec;
+  unordered_map<string, int> unmap;
+  for (int i = 0; i < N; i++) {
+    string v;
+    cin >> v;
+    vec.push_back(v);
+    unmap.insert({v, i});
+  }
+
+  for (int i = 0; i < M; i++) {
+    string v;
+    cin >> v;
+
+    try {
+      auto index = stoi(v);
+      cout << vec[index - 1] << "\n";
+    } catch (...) {
+      cout << unmap[v] + 1 << "\n";
+    }
+  }
+
+  return 0;
+}
