46c197ccbffe5c52c98fe5da264ec3240b2b543a
parkgang <ruddms936@naver.com>
2024-01-23 12:27:45 +0900

    feat: 4134

---
+++ src/baekjoon/silver/4134-다음 소수/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+bool isPrime(long long num) {
+  if (num <= 1) return false;
+
+  if (num == 2 || num == 3) return true;
+
+  if (num % 2 == 0 || num % 3 == 0) return false;
+
+  for (long long i = 5; i * i <= num; ++i) {
+    if (num % i == 0 || num % (i + 2) == 0) return false;
+  }
+
+  return true;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  long long N, n;
+  cin >> N;
+
+  for (int i = 0; i < N; ++i) {
+    cin >> n;
+    while (!isPrime(n)) n++;
+    cout << n << "\n";
+  }
+}
